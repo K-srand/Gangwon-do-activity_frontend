7d353c5f16f93f79f96537cd55fb776c1c55c9b7
JungSeok <jungsuk7083@gmail.com>
2024-07-02 09:24:42 +0900

    community on progress

--- src/components/specific/Community.js
+++ src/components/specific/Community.js
@@ -7,15 +7,13 @@ import '../../assets/styles/Community.css';
 function Community() {
   const [posts, setPosts] = useState([]); // 상태로 posts 관리
 
-  // 데이터 가져오기
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const response = await axios.get('http://localhost:4040/api/v1/board/list');
-        // response.data를 posts 상태로 설정
         const fetchedPosts = response.data.map(post => ({
           id: post.boardNo,
-          author: `사용자${post.userNo}`, // 필요에 따라 사용자 이름을 변경
+          author: post.userNick, // userNick을 author로 설정
           content: post.content,
           date: post.writtenTime.split('T')[0], // 날짜만 사용
           notice: false, // 백엔드 데이터에 따라 설정
@@ -30,19 +28,16 @@ function Community() {
     fetchPosts();
   }, []);
 
-  const postCount = posts.length; // 전체 글 수 계산
+  const postCount = posts.length;
 
   return (
     <div className="community-container">
-      {/* 작성글 헤더 */}
       <div className="header">
         <h3>전체글 {postCount}개의 글</h3>
         <LinkContainer className="write-button" to="/post">
           <Nav.Link>글 작성</Nav.Link>
         </LinkContainer>
-      </div>  
-      
-      {/* 글 목록 시작 */}
+      </div>
       <div className="body">
         <h1 className="title">글 목록</h1>
         <h3 className="right">작성일</h3>
@@ -61,8 +56,6 @@ function Community() {
           </div>
         ))}
       </div>
-      
-      {/* 페이지네이션 */}
       <div className="pagination">
         {Array.from({ length: 10 }, (_, i) => i + 1).map(page => (
           <span key={page} className="page-number">{page}</span>
