9e9ff971990f44f3aeb192c3e1a51e547a4b7fb0
KIMYW\kimyw <kimyw9707@gmail.com>
2024-07-10 22:39:02 +0900

    댓글 페이지 번호 구현하기 전

---
+++ gangwonactivity/src/App.js
@@ -0,0 +1,40 @@
+import {React, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//Test상태인 녀석들 -> 아직 components에서 바로 가져다 쓰는녀석들
+import LoginDetail from './components/specific/LoginDetail';
+import Register from './components/specific/Register';
+
+//Page 단위 구성까지 끝난 녀석들 -> pages에서 끌어오기.
+import MainPage from './pages/MainPage';
+import CommunityListPage from './pages/CommunityListPage'; 
+import './App.css';
+import PostBoard from './pages/PostBoardPage';
+
+// BoardDetail 컴포넌트 임포트 추가
+import BoardDetail from './components/specific/BoardDetail';
+import BoardDetailPage from './pages/BoardDetailPage'; 
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+          {/* <Navbar/> */}
+          {/* <LoginDetail/> */}
+          {/* <Footer/> */}
+          <Routes>
+            <Route path= "logindetail" element={<LoginDetail />}/>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/community" element={<CommunityListPage/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/BoardDetail/:boardNo" element={<BoardDetailPage/>}/>
+            <Route path="/post" element={<PostBoard/>}/>
+          </Routes>
+        </Router>
+    </div>
+
+  );
+}
+
+export default App;
