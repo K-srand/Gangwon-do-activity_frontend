48417afda719ed6d6768084dbb647d4d3820ee8a
JungSeok <jungsuk7083@gmail.com>
2024-07-11 21:32:54 +0900

    123123

--- src/components/common/axiosSetup.js
+++ src/components/common/axiosSetup.js
@@ -10,7 +10,7 @@ axiosInstance.interceptors.request.use(
     config => {
         const token = localStorage.getItem('token');
         if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
+            config.headers['Authorization'] = `Bearer ${token}`; 
         }
         return config;
     },
