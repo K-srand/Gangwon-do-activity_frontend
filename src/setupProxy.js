7f91915e97ae4d772033af75cbb9e0f5507ee1fc
K-srand <ksuji9278@gmail.com>
2024-07-25 16:35:19 +0900

    dfgfdg

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,12 @@ module.exports = function(app) {
   app.use(
     '/api/v1',
     createProxyMiddleware({
-      // target: 'http://223.130.138.174:4040',	// 서버 URL or localhost:설정한포트번호
-      // changeOrigin: true,
-      origin: 'http://223.130.138.174:3030',
-      credentials: true
+      target: 'http://223.130.138.174:4040', // 서버 URL or localhost:설정한포트번호
+      changeOrigin: true, // 이 옵션을 true로 설정하여 요청의 origin을 대상 URL로 변경
+      onProxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('Access-Control-Allow-Origin', 'http://223.130.138.174:3030'); // 클라이언트의 origin을 명시
+        proxyReq.setHeader('Access-Control-Allow-Credentials', 'true'); // 자격 증명 허용
+      }
     })
   );
 };
