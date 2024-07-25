a7aa185fc49647869784c5d02199ab517a895a06
K-srand <ksuji9278@gmail.com>
2024-07-25 15:23:57 +0900

    sdfdsf

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,8 @@ module.exports = function(app) {
     '/api/v1',
     createProxyMiddleware({
       target: 'http://223.130.138.174:4040',	// 서버 URL or localhost:설정한포트번호
-      changeOrigin: true
+      changeOrigin: true,
+      origin: 'http://223.130.138.174:4040'
     })
   );
 };
