91cb0a4346ec039769a8ce2b1f6e07fcca72a052
K-srand <ksuji9278@gmail.com>
2024-08-29 10:38:01 +0900

    수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api/v1',
     createProxyMiddleware({
-      target: 'http://3.36.27.202:4040', // 서버 URL or localhost:설정한포트번호
+      target: 'http://172.31.3.146:4040', // 서버 URL or localhost:설정한포트번호
       changeOrigin: true // 이 옵션을 true로 설정하여 요청의 origin을 대상 URL로 변경
     })
   );
