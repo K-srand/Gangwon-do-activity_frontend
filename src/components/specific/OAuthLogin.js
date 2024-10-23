e093b4defe67b59803f958d71042eca88514d5a7
K-srand <2125ks@naver.com>
2024-10-23 10:50:02 +0900

    oauth2 테스트

--- src/components/specific/OAuthLogin.js
+++ src/components/specific/OAuthLogin.js
@@ -25,39 +25,40 @@ export default function OAuthLogin() {
     useEffect(() => {
         const { token, expirationTime } = getQueryParams();
 
-        if (!token) return;  // token이 없으면 실행하지 않음
+        console.log('token: ', token);
+        console.log('expirationTime: ', expirationTime);
 
-        // 사용자 데이터 가져오기
-        axios.get(`${API_DOMAIN}/user`, {
-            headers: {
-                'Authorization': `Bearer ${token}`  // Bearer 토큰 방식으로 요청
-            }
-        })
-        .then(function(res) {
-            console.log('User data:', res.data.id);
-            setUserId(res.data.id);  // 사용자 ID 설정
+        if (!token) return;
+
+        // axios.get(`${API_DOMAIN}/user`, {
+        //     headers: {
+        //         'Authorization': `Bearer ${token}`
+        //     }
+        // })
+        // .then(function(res) {
+        //     console.log('User data:', res.data.id);
+        //     setUserId(res.data.id); 
             
-            // 모든 값이 존재할 때만 쿠키와 로컬 스토리지에 저장
-            const now = new Date().getTime();
-            const expires = new Date(now + Number(expirationTime) * 1000); // 만료 시간 계산
+        //     const now = new Date().getTime();
+        //     const expires = new Date(now + Number(expirationTime) * 1000); 
             
-            // 쿠키와 로컬 스토리지에 저장
-            setCookie('accessToken', token, { expires, path: '/', secure: true });
-            localStorage.setItem('token', token);
-            localStorage.setItem('userId', res.data.id); 
-            localStorage.setItem('userRole', 'ROLE_USER');
+        //     setCookie('accessToken', token, { expires, path: '/', secure: true });
+        //     localStorage.setItem('token', token);
+        //     localStorage.setItem('userId', res.data.id); 
+        //     localStorage.setItem('userRole', 'ROLE_USER');
 
-            console.log('Logged in successfully');
+        //     console.log('Logged in successfully');
 
-            // 메인 페이지로 이동
-            navigate('/', { replace: true });  // replace: true로 새로고침 방지
-        })
-        .catch(function(error) {
-            console.error("There was an error fetching user data!", error);
-            // 오류가 발생할 경우, 에러 페이지 또는 로그인 페이지로 리디렉션 처리할 수 있음
-            navigate('/login', { replace: true });
-        });
-    }, [location.search, API_DOMAIN, navigate, setCookie]);
+        //     navigate('/', { replace: true });  
+
+        // })
+        // .catch(function(error) {
+        //     console.error("There was an error fetching user data!", error);
+        //     navigate('/login', { replace: true });
+        // });
+
+        // window.location.reload();
+    }, [API_DOMAIN]);
 
     return <div>OAuth Login in progress...</div>;
 }
